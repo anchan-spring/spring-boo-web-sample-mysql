a55dd0252c3c8bf30d078c0dd97201c5361559a4
anchan-spring <54847228+anchan-spring@users.noreply.github.com>
2019-09-07 12:22:43 +0900

    Update create.sql

--- db/mysql_init/create.sql
+++ db/mysql_init/create.sql
@@ -1,3 +1,4 @@
 CREATE DATABASE IF NOT EXISTS spring_web_sample;
 USE spring_web_sample;
 CREATE TABLE IF NOT EXISTS user (userId varchar(256) NOT NULL PRIMARY KEY, firstName varchar(256), lastName varchar(256));
+COMMIT;
